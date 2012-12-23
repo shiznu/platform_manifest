9ebeef5b48d00dbd47a4722dd1029b9cc6b752f6
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-12-23 00:36:46 -0500

    update fw/base pick to PS2

--- pick.sh
+++ pick.sh
@@ -1,7 +1,7 @@
 #run me for frameworks picks
 
 cd ../frameworks/base
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/33/4633/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/33/4633/2 && git cherry-pick FETCH_HEAD
 
 cd ../av
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_av refs/changes/34/4634/1 && git cherry-pick FETCH_HEAD
